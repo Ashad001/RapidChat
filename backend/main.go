a82ba42a093f9d48b81ae0f9bac24e8134848d47
Ashad <ashad001sp@gmail.com>
2024-04-24 19:56:23 +0500

    go pkgs inititated

--- backend/main.go
+++ backend/main.go
@@ -2,43 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"github.com/gorilla/websocket"
+
+	"github.com/Ashad001/RapidChat/pkg/websocket"
+	
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
 
-	CheckOrigin: func(r *http.Request) bool { return true},
-}
-
-func reader(conn *websocket.Conn) {
-	for {messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return 
-		}
-		fmt.Println(string(p))
-		if err := conn.WriteMessage(messageType, p); 
-		err != nil {
-			log.Println(err)
-			fmt.Println("Error", err)
-			return
-		}
-	}
-}
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Host)
 
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
-		fmt.Println(err)
+		fmt.Fprintf(w, "%+V\n", err)
 	}
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func serupRoutes() {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,6 @@ class App extends Component {
     sendMsg("Hello")
   }
 
-
   componentDidMount() {
     connect((msg) => {
       console.log("New Message")
