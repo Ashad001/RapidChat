b5c04ca8d6d4bdd884a9fad86823fbe9f071e4cf
Ashad <ashad001sp@gmail.com>
2024-04-28 15:04:21 +0500

    colors fixed

---
+++ go-backend/pkg/utils/utils.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+    "math/rand"
+    "time"
+)
+
+const (
+	//TODO: Make it automated and generate random colors for more users...
+    Turquoise = "#1ABC9C"
+    Orange    = "#E67E2A"
+    Red       = "#E92750"
+    Blue      = "#3498DB"
+    Green     = "#2ECC71"
+    Yellow    = "#F1C40F"
+    Purple    = "#9B59B6"
+    Gray      = "#514652"
+)
+
+var colors = []string{Turquoise, Orange, Red, Blue, Green, Yellow, Purple, Gray}
+var shuffledColors = make([]string, len(colors))
+var index = 0
+var rng *rand.Rand
+
+func init() {
+    rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+    shuffleColors()
+}
+
+func shuffleColors() {
+    copy(shuffledColors, colors)
+
+    rng.Shuffle(len(shuffledColors), func(i, j int) {
+        shuffledColors[i], shuffledColors[j] = shuffledColors[j], shuffledColors[i]
+    })
+}
+
+func GetRandomColor() string {
+    if index >= len(colors) {
+        shuffleColors()
+        index = 0
+    }
+
+    color := shuffledColors[index]
+    index++
+    return color
+}
